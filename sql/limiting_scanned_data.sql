05f24fbde4628a1650ac55c9a152714b274e593b
edis <61211582+edisedis777@users.noreply.github.com>
2025-03-10 10:23:58 +0100

    Delete sql/limiting_scanned_data.sql

--- sql/limiting_scanned_data.sql
+++
@@ -1,9 +0,0 @@
--- Example: Limiting scanned data using a WHERE clause on a partitioned column
-
-SELECT
-  *
-FROM
-  `your_project.your_dataset.your_partitioned_clustered_table`
-WHERE
-  DATE(event_timestamp) = '2024-10-27' -- Filter by a specific date
-  AND user_id = 'user'; -- Filter by user ID
