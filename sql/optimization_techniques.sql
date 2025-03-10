ab4c092b5c9b8ead6cc7e1464c7d299d9e9efb32
edis <61211582+edisedis777@users.noreply.github.com>
2025-03-10 10:18:12 +0100

    Update optimization_techniques.sql

--- sql/optimization_techniques.sql
+++ sql/optimization_techniques.sql
@@ -1,1 +1,15 @@
+-- Example: Creating a partitioned and clustered table
 
+CREATE OR REPLACE TABLE `your_project.your_dataset.your_partitioned_clustered_table`
+PARTITION BY DATE(event_timestamp) -- Partition by date
+CLUSTER BY user_id -- Cluster by user ID
+AS
+SELECT
+  event_timestamp,
+  user_id,
+  event_type,
+  event_data
+FROM
+  `your_project.your_dataset.your_raw_data_table`
+WHERE
+  event_timestamp IS NOT NULL; -- Filter out null timestamps
